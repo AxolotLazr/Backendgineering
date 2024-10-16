a555654cbac549094c1e42e024b8821d9898c79b
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-10-15 21:37:30 -0700

    gninging

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 let root = document.querySelector(':root');
+
+let browser = getBrowser();
+let browserTheme = getBrowserTheme();
+console.log('Browser Prediction: ' + browser);
+console.log('Browser Theme Prediction: ' + browserTheme);
+
+function getBrowser () {
+    if ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0){
+        return 'Opera';
+    } else if (typeof InstallTrigger !== 'undefined') {
+        return 'Firefox';
+    } else if (/constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification))) {
+        return 'Safari';
+    } else if (/*@cc_on!@*/false || !!document.documentMode) {
+        return 'Internet Explorer';
+    } else if ((!(/*@cc_on!@*/false || (!!document.documentMode) && !!window.StyleMedia)) || ((!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) && (navigator.userAgent.indexOf("Edg") != -1))) {
+        return 'Microsoft Edge';
+    } else if (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) {
+        return 'Google Chrome';
+    } else {
+        return 'unknown';
+    }
+}
+function getBrowserTheme () {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark';
+    } else {
+        return 'light';
+    }
+}
