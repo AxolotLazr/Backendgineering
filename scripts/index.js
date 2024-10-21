84b34b13256fd15fa77fcbf78107918b63dcc6b5
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-10-21 14:54:15 -0700

    Pshew

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,29 @@
 let root = document.querySelector(':root');
 
+// browser theme
 let browser = getBrowser();
-let browserTheme = getBrowserTheme();
-
-let currentVersion = '0.0.3';
-
 console.log('Browser Prediction: ' + browser);
+let browserTheme = getBrowserTheme();
 console.log('Browser Theme Prediction: ' + browserTheme);
 
-console.log('App version estimation: ' + currentVersion)
+// version
+let currentVersion = '0.0.3';
+console.log('App version estimation: ' + currentVersion);
+
+// calculations
+let liquidLoss = gall_to_M3(1000000000000)/4000000000000;
+console.log('Liquid loss: ' + liquidLoss);
+
 document.getElementById('versionDisplay').innerText = '~V'+currentVersion;
 
 // root.style.setProperty('text-shadow', '0px 0px 0.4vmin var(--highlight0)' + ', 0px 0px 1vmin var(--highlight1)'.repeat(3) + ', 0px 0px 3vmin var(--highlight3)'.repeat(6))
+
+fetch("../resources/base/burner.txt")
+    .then((res) => res.text())
+    .then((text) => {
+        console.log(text);
+    })
+    .catch((e) => console.error(e));
 
 function getBrowser () {
     if ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0){
@@ -36,4 +48,8 @@ function getBrowserTheme () {
     } else {
         return 'light';
     }
+}
+
+function gall_to_M3(input){
+    return(input/264.2)
 }
