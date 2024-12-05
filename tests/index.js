d971cbd3ca12fbbb156ae9344a9938950a46d84a
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-12-04 17:36:30 -0700

    Cards
    Now with hands!

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,15 @@
+let cardHolder = document.getElementById('cardHolder');
+log('Got horizontal body');
+
 let card = [];
 
-card.create = function (image, name, description) {
-    let card = document.createElement('div');
-    card.classList = 'card';
+card.newCard = function (image, name, description) {
+    let newCard = document.createElement('div');
+    newCard.classList = 'card';
         let cardImage = document.createElement('img');
         cardImage.src = image;
         cardImage.classList = 'image above';
-        card.appendChild(cardImage);
+        newCard.appendChild(cardImage);
 
 
         let cardBottom = document.createElement('div');
@@ -21,12 +24,37 @@ card.create = function (image, name, description) {
             cardDescription.classList = 'text';
             cardDescription.innerText = description;
             cardBottom.appendChild(cardDescription);
-        card.appendChild(cardBottom);
+        newCard.appendChild(cardBottom);
 
         let cardImageCover = document.createElement('span');
         cardImageCover.classList = 'cardCover';
-        card.appendChild(cardImageCover);
-    body.appendChild(card);
-}
+        newCard.appendChild(cardImageCover);
+    return newCard;
+};
+card.newHand = function () {
+    let newHand = document.createElement('span');
+    newHand.classList = 'hand';
 
-card.create('../images/resources/my-new-character-transparent.png', 'My New Character', "He's just a chill guy who doesn't really give a fuck.");
+        let handCounter = document.createElement('div');
+        handCounter.classList = 'handCounter text';
+        newHand.appendChild(handCounter);
+    return newHand;
+};
+card.addToHand = function (obj, hand) {
+    hand.appendChild(obj);
+
+    for (i = 0; i < hand.children.length; i++) {
+        hand.children[i].style.transform = 'rotate(' + (30/hand.children.length) * (i - 1 - hand.children.length/8) + 'deg) translate(' + (128/hand.children.length) * (i - 1 - hand.children.length/8) + 'px, ' + (-32/hand.children.length) * (i - 1 - hand.children.length/8) + 'px)';
+        hand.children[i].style.zIndex = hand.children.length - 1 * i;
+    };
+    hand.getElementsByClassName('handCounter')[0].innerText = hand.children.length - 1;
+    hand.getElementsByClassName('handCounter')[0].style.transform = '';
+};
+
+appendAtoB(card.newCard("../SVG's/resources/coal.svg", 'Coal', "Quite useful indeed"), cardHolder);
+appendAtoB(card.newCard("", '', "Roughly equal to"), cardHolder);
+
+let hand = card.newHand();
+appendAtoB(hand, cardHolder);
+card.addToHand(card.newCard("../SVG's/resources/polution.svg", 'Polution', "A common byproduct"), hand);
+card.addToHand(card.newCard("../SVG's/resources/polution.svg", 'Polution', "A common byproduct"), hand);
